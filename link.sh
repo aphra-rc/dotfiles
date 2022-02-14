0388c3825bf3d1f4f9172cd9276fc3d8e575ba9e
float-64 <j.andrepb@gmail.com>
2022-02-14 12:48:10 +0000

    Fix link script

--- link.sh
+++ link.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-ln -s $HOME/.config/nvim .config/nvim
-ln -s $HOME/.vim .config/nvim
-ln -s $HOME/.tmux.conf .tmux.conf
+nvim_install_path="$HOME/.config/nvim"
+vim_install_path="$HOME/.vim"
+tmux_install_path="$HOME/.tmux.conf"
+
+rm -f $nvim_install_path
+rm -f $vim_install_path
+rm -f $tmux_install_path
+
+ln -s $PWD/config/nvim $nvim_install_path
+ln -s $PWD/config/nvim $vim_install_path
+ln -s $PWD/tmux.conf $tmux_install_path
